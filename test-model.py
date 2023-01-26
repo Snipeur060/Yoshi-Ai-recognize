12ce8c9b04cf3791e20509856d76cfd75400938a
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2023-01-26 18:58:52 +0100

    Create test-model.py

---
+++ test-model.py
@@ -0,0 +1,14 @@
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+
+# Charger le modèle
+model = load_model('yoshi_model_keras.h5')
+
+# Charger les données d'évaluation
+eval_data = np.array([image.img_to_array(image.load_img('eval_data/image1.jpg', target_size=(224, 224)))])
+eval_labels = np.array([0]) # 0 pour Yoshi, 1 pour rien
+
+# Évaluation du modèle
+eval_loss, eval_acc = model.evaluate(eval_data, eval_labels)
+print(f'Evaluation Loss (taux d\'echec) : {eval_loss}, Evaluation Accuracy (taux de réussite): {eval_acc}')
